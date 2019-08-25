caa15003bf8edf6617e66024765d1e7cd6ad1c12
Daniel Pennypacker <pennypacker@gmail.com>
2019-08-25 11:43:31 -0700

    adding port

--- index.js
+++ index.js
@@ -4,5 +4,4 @@ const app = express()
 const port = 3000
 
 app.use(express.static('public'))
-
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.listen(process.env.PORT || port, () => console.log(`Example app listening on port ${port}!`))
